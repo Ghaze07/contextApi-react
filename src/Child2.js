ddefac8237b7f5e737edad2eba62e931ad720ae0
Ghaze07 <68465222+Ghaze07@users.noreply.github.com>
2020-08-07 08:46:02 +0500

    update app to manage state with reducer

---
+++ src/Child2.js
@@ -0,0 +1,20 @@
+import React, { useContext, useReducer } from 'react';
+import ValueContext from './ValueContext'
+import numberReducer from './numberReducer';
+
+
+function Child2() {
+    
+    let [ state, dispatch ] = useReducer(numberReducer, 52);
+    
+  return (
+    <div>
+      Child2 {state}
+      <br />
+      <button onClick={() =>{ dispatch({type:"INCREMENT", val :67})}}>Increment</button>
+      <button onClick={() =>{ dispatch({type:"DECREMENT", val: 34})}}>Decrement</button>
+    </div>
+  );
+}
+
+export default Child2;
