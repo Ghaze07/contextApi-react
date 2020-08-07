b2b77473f918537db3025b387a974b4b1d8869c4
Ghaze07 <68465222+Ghaze07@users.noreply.github.com>
2020-08-07 09:20:44 +0500

    Update Child2.js

--- src/Child2.js
+++ src/Child2.js
@@ -1,5 +1,5 @@
-import React, { useContext, useReducer } from 'react';
-import ValueContext from './ValueContext'
+import React, { useReducer } from 'react';
+//import ValueContext from './ValueContext'
 import numberReducer from './numberReducer';
 
 
@@ -18,3 +18,4 @@ function Child2() {
 }
 
 export default Child2;
+
